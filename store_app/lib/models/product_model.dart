936ecee042eda8cb19c8770bd57e98f4804e47c1
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-03-16 19:16:55 +0300

    new updates

--- store_app/lib/models/product_model.dart
+++ store_app/lib/models/product_model.dart
@@ -4,6 +4,7 @@ class ProductModle {
   final double price;
   final String description;
   final String image;
+  final RatingModel rating;
 
   ProductModle({
     required this.id,
@@ -11,6 +12,7 @@ class ProductModle {
     required this.price,
     required this.description,
     required this.image,
+    required this.rating,
   });
 
   factory ProductModle.fromJson(jsonData) {
@@ -20,6 +22,21 @@ class ProductModle {
       price: jsonData['price'],
       description: jsonData['description'],
       image: jsonData['image'],
+      rating: RatingModel.fromJson(jsonData['rating']),
+    );
+  }
+}
+
+class RatingModel {
+  //
+  final double rate;
+  final int count;
+
+  RatingModel({required this.rate, required this.count});
+  factory RatingModel.fromJson(jsonData) {
+    return RatingModel(
+      rate: jsonData['rate'],
+      count: jsonData['count'],
     );
   }
 }
--- store_app/lib/services/get_all_product_services.dart
+++ store_app/lib/services/get_all_product_services.dart
@@ -1,4 +1,20 @@
-class GetAllProducts{
+import 'dart:convert';
 
-  
+import 'package:store_app/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class AllProductsService {
+  Future<List<ProductModle>> getAllProducts() async {
+    http.Response response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    //
+    List<dynamic> data = jsonDecode(response.body);
+    //
+    List<ProductModle> productsList = [];
+    //
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(ProductModle.fromJson(data[i]));
+    }
+    return productsList;
+  }
 }
